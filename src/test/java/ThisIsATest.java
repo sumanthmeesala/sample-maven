96d8de493a4d6a514cfa572b61dd27b5253f6bff
Sumanth <sumanth@gmail.com>
2023-07-23 14:49:22 +0530

    Added a sample test case

---
+++ src/test/java/ThisIsATest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ThisIsATest {
+
+	@Test
+	public void test_this() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void test_that() {
+		assertTrue(true);
+	}
+
+}
